fcd3ae4fdc41e4ed9b32a9e975bda6bd12e8c470
AnthonyArminio <tonyarminio.0@gmail.com>
2025-08-17 14:21:56 -0400

    added Test script

---
+++ Test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+button = st.button("Test Button")
+reset_button = st.button("Reset")
+
+if (button):
+    st.session_state.count += 1
+
+if (reset_button):
+    st.session_state.count = 0
+
+for i in range(st.session_state.count):
+    st.write("The button was pressed.")
