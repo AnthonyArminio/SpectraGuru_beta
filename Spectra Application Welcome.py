a3ae4f5a1b63cd673478c1bae767034e7cc8e3e9
Anthony Arminio <tonyarminio.0@gmail.com>
2025-09-24 13:54:57 -0400

    created template for secrets file

--- Spectra Application Welcome.py
+++ Spectra Application Welcome.py
@@ -15,6 +15,16 @@ os.chdir(current_dir)
 
 function.wide_space_default()
 
+# Request authentication
+if not st.user.is_logged_in:
+    if st.button("Log in with Google"):
+        st.login()
+    st.stop()
+
+if st.button("Log out"):
+    st.logout()
+
+
 # hide_st_style = """
 #             <style>
 #             #MainMenu {visibility: hidden;}
